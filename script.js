d4f62ffec880efe335b55aee681b81e1393c8659
akuma-dhruv <dhruvgarg79@gmail.com>
2022-09-09 01:31:04 +0530

    Complete

--- script.js
+++ script.js
@@ -79,36 +79,35 @@ function showResult(score, playerChoice, computerChoice) {
 
     let result = "";
     let divID = document.getElementById("result");
-
+    
     score = getResult(playerChoice, computerChoice);
     if (score === 0)
-        result = "It's a Draw!";
+    result = "It's a Draw!";
     else if (score === -1)
-        result = 'You Lose!';
+    result = 'You Lose!';
     else
-        result = 'You Win!';
-
+    result = 'You Win!';
+    
     divID.innerText = result;
     return score;
 }
 
 // ** Calculate who won and show it on the screen **
 function onClickRPS(playerChoice) {
-
-    
-    let computerChoice = getComputerChoice();
-    
+    let computerChoice = getComputerChoice();    
     return showResult(0, playerChoice, computerChoice);
-     
+    
 }
 
 
 // ** Make the RPS buttons actively listen for a click and do something once a click is detected **
 function playGame() {
     const rpsButtons = document.querySelectorAll(".rpsButton");
+    const endButton=document.querySelector('#endGameButton');
     var handsDiv = document.getElementById('hands');
     var  playerScoreDiv= document.getElementById('player-score');
     let score, hands=0,playerScore=0;
+    
     rpsButtons.forEach(rpsButton => {
         rpsButton.onclick = () => {
             score= onClickRPS(rpsButton.value);
@@ -117,15 +116,22 @@ function playGame() {
             playerScoreDiv.innerText ="Score: "+ playerScore;
             handsDiv.innerText = "Hands: "+hands;
         }});
-
-
-
-
-}
-
-// ** endGame function clears all the text on the DOM **
-function endGame() {
-
-}
+        endButton.onclick= ()=>{
+            endGame();
+            playerScoreDiv.innerText ="Score: 0";
+            handsDiv.innerText = "Hands: 0";
+            
+        }
+        
+        
+        
+    }
+    
+    // ** endGame function clears all the text on the DOM **
+    function endGame() {
+        let divID = document.getElementById("result");
+        divID.innerText = "";
+        
+    }
 
 playGame()
